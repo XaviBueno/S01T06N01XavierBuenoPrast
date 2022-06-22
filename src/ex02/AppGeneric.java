13ba8cc6c425f458dd52aa43e0fa6079f9430e89
Xavi <xbuen@192.168.1.47>
2022-06-22 13:34:32 +0200

    Entregat

--- src/ex02/AppGeneric.java
+++ src/ex02/AppGeneric.java
@@ -3,7 +3,7 @@ package ex02;
 public class AppGeneric {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 		Persona persona1=new Persona("Josep","Garcia", 30);
 		GenericMethods.llistaObjectes(persona1,"Hola",3);
 		Persona persona2=new Persona("Ana","Martinez", 17);
